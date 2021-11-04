7efc42d3ee01e6190a85fca2ed064e216fa029e1
rikyhidayat21 <rikyhidayat21@gmail.com>
2021-11-04 21:12:50 +0700

    query pokemon list, and create component Pokemon List and Pokemon Card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import PokemonList from "../components/PokemonList";
+import axios from "../config/axios";
 
 export default function HomePage() {
-  return <div>HOmePage</div>;
+  const [pokemons, setPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=20"
+  );
+
+  useEffect(() => {
+    retrievePokemons();
+  }, []);
+
+  const retrievePokemons = async () => {
+    try {
+      const { data } = await axios.get("pokemon?limit=20");
+
+      setLoadMore(data.next);
+
+      const getPokemonStats = (pokemons) => {
+        pokemons.forEach(async (pokemon) => {
+          const { data } = await axios.get(`pokemon/${pokemon.name}`);
+          setPokemons((currentList) => [...currentList, data]);
+        });
+      };
+
+      getPokemonStats(data.results);
+      // await console.log(pokemons);
+    } catch (error) {
+      console.log(error, "<== error retrieve");
+    }
+  };
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          {pokemons &&
+            pokemons.map((pokemon) => (
+              <PokemonList
+                key={pokemon.id}
+                id={pokemon.id}
+                name={pokemon.name}
+                image={pokemon.sprites.other.dream_world.front_default}
+                type={pokemon.types[0].type.name}
+              />
+            ))}
+        </Row>
+      </Container>
+    </div>
+  );
 }
