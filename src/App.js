73e80ce3f9f73eef11cd37021f5a2591c0c4ad38
rikyhidayat21 <rikyhidayat21@gmail.com>
2021-11-04 16:11:13 +0700

    configuration axios and set up react-router-dom
    Create axios baseURL configuration, and set up react router dom for HomePage and DetailPage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import DetailPage from './pages/DetailPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/pokemon/:name" element={<DetailPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
